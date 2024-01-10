469ae24f3fc0f5718944841af661a2bd62aafc3f
GBastienDev <gambet.bastien.dev@gmail.com>
2024-01-10 14:59:49 +0100

    bug d'import jwt

--- src/components/Profil/Profil.jsx
+++ src/components/Profil/Profil.jsx
@@ -3,7 +3,7 @@ import ModalGenre from './ModalGenre/ModalGenre'
 import ModalPlatform from './ModalPlatform/ModalPlatform'
 import React, {useState, useEffect} from "react"
 import { useNavigate } from 'react-router-dom'
-import jwt_decode from "jwt-decode"
+import * as jwt_decode from "jwt-decode"
 
 const Profil = () => {
 
