ec5110b1669712ca92b1975eefee739480d153dd
GBastienDev <gambet.bastien.dev@gmail.com>
2024-01-10 10:04:21 +0100

    structure gamedetails

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -3,8 +3,17 @@ import './GameDetails.scss'
 const GameDetails = () => {
 
     return(
-        <>
-        </>
+        <div className='game'>
+            <img className="game__img"src="background-image"  alt="" />
+            <h1 className="game__title">titre jeu</h1>
+            <p className="game__release">date de sortie</p>
+            {/* <p className="game__platform">plateforme</p>   ou logo ou les 2          */}
+            <p className="game__genre">genre</p>
+            <div className="game__meta">note meta</div>
+            <p className="game__desc">description_raw</p>
+            <p className="game__publisher">publisher</p>
+            <p className="game__dev">developper</p> 
+        </div>
     )
 }
 
