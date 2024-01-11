3b0d831d152756f383942563229b4fd797e7a5b8
LauraGalouye <lgalouye@gmail.com>
2024-01-11 17:50:47 +0100

    page gamedetails en cours

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -1,20 +1,37 @@
 import './GameDetails.scss'
+import { useParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react'
+import { HomeContext } from '../../context/HomeContext'
 
 const GameDetails = () => {
 
+    const {id} = useParams();
+    console.log(id)
 
+    const [popular, setPopular] = useContext(HomeContext)
+    const [gameDetails, setGameDetails]= useState('')
+
+    const findGame = async () => {
+        const selectedGame = await popular.results.find((game => game.id === parseInt(id)))
+        setGameDetails(selectedGame)
+    }
+
+    useEffect(()=>{
+        findGame()
+    }, [])
     //page détail du jeu avec image en fond   
     return(
         <div className='game'>
-            <img className="game__img"src="background-image"  alt="" />
-            <h1 className="game__title">titre jeu</h1>
-            <p className="game__release">date de sortie</p>
-            {/* <p className="game__platform">plateforme</p>   ou logo ou les 2          */}
-            <p className="game__genre">genre</p>
+            {}
+            <img className="game__img"src={gameDetails.background_image}  alt={gameDetails.name} />
+            <h1 className="game__title">{gameDetails.name}</h1>
+            <p className="game__release">{gameDetails.released}</p>
+            
+            <p className="game__genre">{gameDetails.genres}</p>
             <div className="game__meta">note meta</div>
             <p className="game__desc">description_raw</p>
-            <p className="game__publisher">publisher</p>
-            <p className="game__dev">developper</p> 
+            <p className="game__publisher">{gameDetails.publisher}</p>
+            <p className="game__dev">{gameDetails.developper}</p> 
             <img src="" alt="" />  
         </div>
     )
