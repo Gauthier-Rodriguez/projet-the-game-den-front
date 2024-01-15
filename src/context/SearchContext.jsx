571a76c8d6c81128c3e30c29f748174ea5216af3
GBastienDev <gambet.bastien.dev@gmail.com>
2024-01-15 10:26:17 +0100

    filtre dans le call api

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -14,9 +14,19 @@ export const SearchController = ({children}) => {
 
     const fetchSearch = async () => {
         try{
-            const apiCall = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&search=${search}&search_precise=true&ordering=-released`)
+            const apiCall = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&search=${search}&search_exact=true&ordering=-released&exclude_stores=9,8,4&parent_platforms=1,2,3,7`)
             setSearch(apiCall.data)
             console.log(apiCall.data)
+    
+
+            const sortedResults = apiCall.data.results.sort((a, b) => {
+                const releaseDateA = new Date(a.released).getTime() || 0;
+                const releaseDateB = new Date(b.released).getTime() || 0;
+                return releaseDateB - releaseDateA;
+            });
+
+            setSearch({ ...apiCall.data, results: sortedResults });
+    
         } 
         catch (error){
             setError(error)
