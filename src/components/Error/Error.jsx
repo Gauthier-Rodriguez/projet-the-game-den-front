87ae07ab36f1860fe98ef23c1ae6347be2016542
GBastienDev <gambet.bastien.dev@gmail.com>
2024-01-29 13:22:57 +0100

    test zoom

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
         <>
           <h1>ERROR 404 ou 500</h1>
           <p>There was a problem</p>
-          <img src="https://softwaretested.com/wp-content/uploads/2018/12/System-Error.jpg" alt="error image"/>
+          <img src="" alt="error image"/>
           <Link className="error__link" to="/">Back to homepage</Link>
         </>
     )
